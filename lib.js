8d334d5acdc7e09e5436d3768aca69daf341dca6
leesju0103 <dltmdwn0322@gmail.com>
2022-10-11 18:35:44 +0900

    first commit

---
+++ lib.js
@@ -0,0 +1,26 @@
+function sum(numbers) {
+    return numbers.reduce((prev, curr) => prev + curr, 0)
+    // 1.
+    // let s = 0;
+    // for(let i=0; i<numbers.length; i++){
+    //     s += numbers[i];
+    // }
+    // 2.
+    // numbers.forEach((n) => (s += n;));
+}
+
+function avg(numbers) {
+    return sum(numbers) / numbers.length;
+}
+
+function max(numbers){
+    return numbers.reduce((max, curr) => (max > curr ? max : curr), 0)
+}
+
+exports.sum = sum;
+localStorage
+module.exports = {
+    sum,
+    avg,
+    max,
+};
